6bb725c20c1b9a4b66ad837c5ed21aa57f9d8e0d
minjun7984 <54847941+minjun7984@users.noreply.github.com>
2021-08-26 23:57:14 +0900

    POST API 연습

---
+++ main/java/com/example/hello/controller/PostApiController.java
@@ -0,0 +1,20 @@
+package com.example.post.controller;
+
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData){ //RequstBody통해 json 파싱 가능
+
+        System.out.println(requestData);
+    }
+}
