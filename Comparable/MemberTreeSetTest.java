9fa13c100b24fc9a72870277df327127bea534b3
minjun7984 <54847941+minjun7984@users.noreply.github.com>
2021-09-18 21:55:44 +0900

    Comparable 구현
    객체를 정렬(오름차순, 내림차순)하는 compararble 구현

---
+++ Comparable/MemberTreeSetTest.java
@@ -0,0 +1,28 @@
+package Comparable;
+
+import Comparable.Member;
+import HashSet.MemberHashSet;
+
+import java.util.TreeSet;
+
+public class MemberTreeSetTest {
+
+    public static void main(String[] args){
+
+        MemberTreeSet memberTreeSet = new MemberTreeSet();
+
+        Member memberHong= new Member(1004,"홍민준");
+        Member memberJung = new Member(1001, "정민준");
+        Member memberKim = new Member(1002, "김민준");
+        Member memberLee = new Member(1003, "이민준");
+
+        memberTreeSet.addMember(memberHong);
+        memberTreeSet.addMember(memberKim);
+        memberTreeSet.addMember(memberJung);
+        memberTreeSet.addMember(memberLee);
+
+
+        memberTreeSet.showAllMember();
+
+    }
+}
