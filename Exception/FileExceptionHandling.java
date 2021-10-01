ae7ba8e2176f8b02c5a608be90d5cc0c92a72c4f
minjun7984 <54847941+minjun7984@users.noreply.github.com>
2021-10-02 05:23:26 +0900

    예외처리 연습

---
+++ Exception/FileExceptionHandling.java
@@ -0,0 +1,19 @@
+package Exception;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExceptionHandling {
+
+    public static void main(String[] args){
+
+        try (FileInputStream fis = new FileInputStream("a.txt")){
+            System.out.println("read");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("end");
+    }
+}
