982d581b280e2a7bcbc2c02ed18218b8274375cc
minjun7984 <54847941+minjun7984@users.noreply.github.com>
2021-10-02 05:23:06 +0900

    예외처리 연습

---
+++ Exception/AutoCloseTest.java
@@ -0,0 +1,16 @@
+package Exception;
+
+public class AutoCloseTest {
+
+    public static void main(String[] args){
+
+        AutoCloseable obj = new AutoCloseableObj();
+
+        try(obj){
+            throw new Exception();
+        }catch(Exception e){
+            System.out.println("exception");
+        }
+        System.out.println("end");
+    }
+}
