063786e30fd39995ad82e0c62e762674491bfda7
minjun7984 <54847941+minjun7984@users.noreply.github.com>
2021-10-06 19:58:51 +0900

    오류 로그 남기기 Logger이용

---
+++ Exception/LoggerTest.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class LoggerTest {
+
+    public static void main(String[] args){
+
+        MyLogger logger = MyLogger.getLogger();
+        logger.log("log test");
+        }
+}
